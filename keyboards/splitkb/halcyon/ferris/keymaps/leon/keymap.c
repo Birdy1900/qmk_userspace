83fe41f83f289e0ac95757eb017faa92ccf893f9
Birdy1900 <leon.loiseau@protonmail.com>
2025-06-29 20:44:50 +0200

    Update keymap.c

--- keyboards/splitkb/halcyon/ferris/keymaps/leon/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/leon/keymap.c
@@ -66,7 +66,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
 	case ASSS:
       if (record->event.pressed) {
-        SEND_STRING("Ns;75mx1QQQQ");
+        SEND_STRING("Ns;75mx1");
       } else {
       }
       break;
